6676eb789c98252a1fa08f2a4574af29703a9e10
kgugunava <k.gugunava01@gmail.com>
2025-10-21 17:49:31 +0300

    Fix merge

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-<<<<<<< HEAD
 	"fmt"
 
 	"github.com/kgugunava/gorkycode_backend/internal/adapters/postgres"
@@ -15,15 +14,4 @@ func main() {
 	db.ConnectToDatabase(app.Cfg)
 	db.CreateDatabase(app.Cfg)
 	db.CreateDatabaseTables(app.Cfg)
-=======
-    "fmt"
-     "github.com/kgugunava/gorkycode_backend/internal/app"
-)
-
-func main() {
-    app := app.NewApp()
-    
-    address := fmt.Sprintf("%s:%s", app.Cfg.ServerAddress, app.Cfg.Port)
-    app.Router.Route(address)
->>>>>>> 644e9008b33b660d061f98e2d88827f7b04131c4
 }
